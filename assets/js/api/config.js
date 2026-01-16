15a05a47d331717fc2c805f79fdf45deb0f3ede9
Dhyan Soni <dhyantsoni@gmail.com>
2026-01-16 04:28:51 -0800

    deployment

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -6,7 +6,7 @@ export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     pythonURI = "http://localhost:8405";  // Same URI for localhost or 127.0.0.1
 } else {
-    pythonURI = "https://flaskstu.opencodingsociety.com";
+    pythonURI = "https://hardwarehavoc.opencodingsociety.com";
 
 }
 
